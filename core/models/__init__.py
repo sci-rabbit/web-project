7f8e2ddcfec609076f31d48a35e8292ccbd9c690
Alexandr Beliakov <genzksync0@gmail.com>
2025-07-01 11:34:36 +0300

    Create user model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,8 +3,10 @@ __all__ = (
     "Product",
     "DatabaseManager",
     "db_manager",
+    "User",
 )
 
 from .base import Base
 from .product import Product
 from .session import DatabaseManager, db_manager
+from .user import User
