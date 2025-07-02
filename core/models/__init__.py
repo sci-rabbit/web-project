dc5d929f43df32dd6422779e7d3145cc8088d0f0
Alexandr Beliakov <genzksync0@gmail.com>
2025-07-02 18:31:14 +0300

    Create class UserRelationMixin for implementation User model relations

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -5,6 +5,7 @@ __all__ = (
     "db_manager",
     "User",
     "Post",
+    "UserRelationMixin",
 )
 
 from .base import Base
@@ -12,3 +13,4 @@ from .product import Product
 from .session import DatabaseManager, db_manager
 from .user import User
 from .post import Post
+from .mixin import UserRelationMixin
--- core/models/post.py
+++ core/models/post.py
@@ -3,23 +3,20 @@ from typing import TYPE_CHECKING
 from sqlalchemy import String, Text, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from core.models import Base
+from core.models import Base, UserRelationMixin
 
 if TYPE_CHECKING:
     from .user import User
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_id_nullable: bool = False
+    _user_id_unique: bool = False
+    _user_back_populates: str | None = "posts"
+
     tittle: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(
         Text,
         default="",
         server_default="",
     )
-
-    user_id: Mapped[str] = mapped_column(
-        ForeignKey("users.id"),
-        nullable=False,
-    )
-
-    user: Mapped["User"] = relationship(back_populates="posts")
